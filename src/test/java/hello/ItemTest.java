a7c27aee6b13032bfe048d19f20279f3d72527be
Disha1250 <167982974+Disha1250@users.noreply.github.com>
2024-04-25 21:46:07 +1000

    Update ItemTest.java

--- src/test/java/hello/ItemTest.java
+++ src/test/java/hello/ItemTest.java
@@ -1,1 +1,21 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
+public class ItemTest {
+
+    @Test
+    public void testUpdateItem() {
+        Item item = new Item("TestItem", 10.0, 5, "Test Description", null);
+        item.updateItem("NewName", 15.0, "New Description", null);
+        assertEquals("NewName", item.getName());
+        assertEquals(15.0, item.getPrice(), 0.001); // Delta is used for floating point comparison
+        assertEquals("New Description", item.getDesc());
+    }
+
+    @Test
+    public void testUpdateQuant() {
+        Item item = new Item("TestItem", 10.0, 5, "Test Description", null);
+        item.updateQuant(7);
+        assertEquals(7, item.getQuant());
+    }
+}
