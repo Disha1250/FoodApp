3a4bf62ba3e0db0909d898d58364038804925d11
Disha1250 <167982974+Disha1250@users.noreply.github.com>
2024-04-26 17:53:05 +1000

    Update Item.java

--- src/main/java/hello/Item.java
+++ src/main/java/hello/Item.java
@@ -5,7 +5,7 @@ public class Item {
     private double price;
     private int quant;
     private String desc;
-    private i image;
+    // private i image;
 
     //default constructor
     public Item(){
@@ -13,16 +13,17 @@ public class Item {
         price = 0;
         quant = 0;
         desc = "";
-        image = 0;
+        //image = 0;
     }
 
     //copy constructor
-    public Item(String name, double price, int quant, String desc, i image){
+    //public Item(String name, double price, int quant, String desc, i image){
+    public Item(String name, double price, int quant, String desc){
         this.name = name;
         this.price = price;
         this.quant = quant;
         this.desc = desc;
-        this.image = image;
+        //this.image = image;
     }
 
     //get methods
@@ -42,13 +43,13 @@ public class Item {
         return desc;
     }
 
-    public i getImage(){
-        return image;
-    }
+    //public i getImage(){
+     //   return image;
+    //}
 
     //method for updating info of an item
-    public void updateItem(String newName, double newPrice, String newDesc, i newImage){
-
+    //public void updateItem(String newName, double newPrice, String newDesc, i newImage){
+    public void updateItem(String newName, double newPrice, String newDesc){
     }
 
     //method for updating the quantity of an item
